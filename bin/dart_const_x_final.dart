0c53d1f2a1be2892cfb361e89912160935efeb93
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-02-14 19:45:18 -0300

    Commit 1

---
+++ bin/dart_const_x_final.dart
@@ -0,0 +1,9 @@
+void main(List<String> arguments) {
+  /* const => Variaveis com valores que sao imutaveis, recebe o valor quando a variavel for criada */
+  const String nome1 = "Dart";
+  print(nome1);
+  /* final => Atribuição unica para as variaveis, so recebe um valor, nao precisa receber esse valor quando for criada, depois que recebe um valor ela fica imutavel */
+  final String nome2;
+  nome2 = "Flutter";
+  print(nome2);
+}
---
+++ test/dart_const_x_final_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_const_x_final/dart_const_x_final.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
